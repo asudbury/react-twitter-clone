41a151989f679af9880e6384c0a6e8816524d933
asudbury <sudburya@gmail.com>
2021-06-19 15:52:55 +0000

    Deploying to gh-pages from  @ 3a7d1c9e32e651ddeecc46e5675b99aa6a94f4f9 🚀

---
+++ precache-manifest.a3b0f4dc4e8eb550439c3065842ffa06.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9745527f7dc511bf96b1843c198c734e",
+    "url": "/react-twitter-clone/index.html"
+  },
+  {
+    "revision": "9c9d5881c181d0e3437a",
+    "url": "/react-twitter-clone/static/js/2.92fff3cf.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/react-twitter-clone/static/js/2.92fff3cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d084db2a93d098ed9d3a",
+    "url": "/react-twitter-clone/static/js/main.ea201885.chunk.js"
+  },
+  {
+    "revision": "99319584f391d83f58f8",
+    "url": "/react-twitter-clone/static/js/runtime-main.8e2047ae.js"
+  }
+]);
